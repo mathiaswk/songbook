e2e0c0e19ed15c40c537dd7b672e72a6f83b65e2
Mathias Weirsøe Klitgaard <mathias@weirsoe.com>
2022-03-08 12:41:05 +0100

    temp

--- songbook/__main__.py
+++ songbook/__main__.py
@@ -8,12 +8,23 @@
 import argparse
 
 from songbook import __version__
-from songbook.cli import cli
-from songbook.gui import gui
+# from songbook.cli import cli
+# from songbook.gui import gui
 
 
 def _build_parser():
-    parser = argparse.ArgumentParser(prog='songbook', description='Create a songbook.')
+    parser = argparse.ArgumentParser(prog='songbook', usage='%(prog)s [options] [FILE ...]',
+                        description='Create a songbook.')
+    parser.add_argument('-V', '--version', action='version', version=f'%(prog)s {__version__}')
+    parser.add_argument('--filelist', metavar='FILE', action='append',
+                        help='reads song file names from FILE')
+    parser.add_argument('--transpose', metavar='N', action='store',
+                        help='Transposes by N semi-tones')
+
+    parser.add_argument('--toc', action=argparse.BooleanOptionalAction,
+                        help='generates/suppresses a table of contents')
+
+    parser.add_argument('file', metavar='FILE', nargs='*')
     parser.add_argument(
         '-p', '--style', action='store',
         help="used for making a new pagenumber style")
@@ -35,11 +46,9 @@ def _build_parser():
     parser.add_argument(
         '-t', '--twosided', action='store_true')
     # TODO
-    parser.add_argument('-v', '--version', action='version', version=f'%(prog)s {__version__}')
     parser.add_argument('-i', '--interactive', action='store_true')
     parser.add_argument('--order', choices=['fixed', 'sorted', 'random'])
     parser.add_argument('--seed', action='store')
-    parser.add_argument('song', nargs='*')
     return parser
 
 
@@ -47,10 +56,10 @@ def main():
     parser = _build_parser()
     args = parser.parse_args()
 
-    if not args.cli:
-        cli.run()
-    else:
-        gui.run()
+    # if not args.cli:
+    #     cli.run()
+    # else:
+    #     gui.run()
 
 
 if __name__ == '__main__':
